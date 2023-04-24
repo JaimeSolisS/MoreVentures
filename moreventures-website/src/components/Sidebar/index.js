93d9beda4e50422c548226d76f3bd01770f58abf
JaimeSolisS <jaimesolissoto@me.com>
2023-04-23 21:10:01 -0600

    add Sidebar and Style

---
+++ moreventures-website/src/components/Sidebar/index.js
@@ -0,0 +1,33 @@
+import React from "react";
+import {
+  SidebarContainer,
+  Icon,
+  CloseIcon,
+  SidebarWrapper,
+  SidebarMenu,
+  SidebarLink,
+  SidebarBtnWrap,
+  SidebarRoute,
+} from "./SidebarElements";
+
+function Sidebar() {
+  return (
+    <SidebarContainer>
+      <Icon>
+        <CloseIcon />
+      </Icon>
+      <SidebarWrapper>
+        <SidebarMenu>
+          <SidebarLink to="about">About</SidebarLink>
+          <SidebarLink to="services">Services</SidebarLink>
+          <SidebarLink to="contact">Contact</SidebarLink>
+        </SidebarMenu>
+        <SidebarBtnWrap>
+          <SidebarRoute to="/signin">Sign In</SidebarRoute>
+        </SidebarBtnWrap>
+      </SidebarWrapper>
+    </SidebarContainer>
+  );
+}
+
+export default Sidebar;
